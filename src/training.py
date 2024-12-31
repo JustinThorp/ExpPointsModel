07f9ba21b213785c533f7d4a5aea3712d25daee2
Justin Thorp <jjtthorp@gmail.com>
2024-12-30 22:16:04 -0700

    Created training loop

--- src/training.py
+++ src/training.py
@@ -1,5 +1,7 @@
 import polars as pl
 import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
 
 raw_data = pl.scan_parquet(
     "/Users/justinthorp/Documents/NFLWinProb/data/traing_data.parquet"
@@ -33,8 +35,65 @@ data = (
             .alias("total_def_score"),
         ]
     )
-    .with_columns([])
+    .drop_nulls()
     .collect()
 )
 
 print(data)
+
+
+class TrainingData(Dataset):
+    def __init__(self, features: pl.DataFrame, target: pl.DataFrame) -> None:
+        super().__init__()
+        self.features = torch.tensor(features.to_numpy(), dtype=torch.float32)
+        self.target = torch.tensor(target.to_numpy(), dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, index):
+        return self.features[index], self.target[index]
+
+
+train_dataset = TrainingData(
+    data[["down", "yardline_100", "game_seconds_remaining", "total_pos_score"]],
+    data[["pos_score"]],
+)
+train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+
+class EPModel(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.BatchNorm1d(4),
+            nn.Linear(4, 10),
+            nn.ReLU(),
+            nn.Linear(10, 10),
+            nn.ReLU(),
+            nn.Linear(10, 1),
+        )
+
+    def forward(self, x):
+        x = self.layers(x)
+        return x
+
+
+net = EPModel()
+
+criterion = nn.MSELoss()
+optim = torch.optim.Adam(net.parameters(), lr=0.1)
+
+
+for i in range(10):
+    avg_loss = 0
+    i = 0
+    for X, y in train_dataloader:
+        pred = net(X)
+        loss = criterion(y, pred)
+        loss.backward()
+        avg_loss += loss.item()
+        i += 1
+    optim.step()
+    optim.zero_grad()
+    print(avg_loss / i)
